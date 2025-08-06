e14a190cf1dc1cf14502c8dccac5f2b0df60f315
Gie19 <ragiealmachar@gmail..com>
2025-08-06 14:15:50 +0800

    added cors dependency for better connectivity of API request

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
+
 
 
 //Routers
@@ -31,12 +33,14 @@ const { read } = require('fs');
 var app = express();
 
 
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
